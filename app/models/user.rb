7b7a099e56c899d5b24f3ab09dba5b1421b50502
vasya-babiy <basek.93@gmail.com>
2014-05-05 14:19:39 +0300

    test

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   attr_accessible :email, :password, :password_confirmation
 
   attr_accessor :password
+  lala
   before_save :encrypt_password
 
   validates_confirmation_of :password
